f2f89eaed1e252d692d6f6944befe52eba3f0468
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-22 16:23:39 +0100

    created new test

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 feature 'Starting a new game' do
+
   scenario 'Starting a new game' do
     visit '/'
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+
+  scenario 'New game page can accept Player name' do
+    visit '/new_game'
+    fill_in 'Player 1 Name', :with => 'David'
+    click_button 'Go'
+    expect(page).to have_content 'Success'
+  end
+
+
+
 end
