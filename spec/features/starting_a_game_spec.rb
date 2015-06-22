da65a743f711a861c46e4fbaeadfd1b933a5dd00
DuskyShelf <dupsdale@gmail.com>
2015-06-22 15:16:27 +0100

    added starting_a_game_spec.rb

---
+++ spec/features/starting_a_game_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'Starting a new game' do
+  scenario 'Starting a new game' do
+    visit '/'
+    click_link 'New Game'
+    expect(page).to have_content "What's your name?"
+  end
+end
